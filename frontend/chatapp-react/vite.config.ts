23dbf6b8e7df1f45f2c17d32cdb017eb14e60516
Mengenes <mengene222s@gmail.com>
2026-05-16 12:55:37 +0300

    build fix frontend

--- frontend/chatapp-react/vite.config.ts
+++ frontend/chatapp-react/vite.config.ts
@@ -1,13 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_PATH || "/chatapp-react",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+  };
+});
